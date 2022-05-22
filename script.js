f41ff2fdaa017087384117d9d533c8116936e12a
Leonardo Fukuje <96839904+leonardo-fukuje@users.noreply.github.com>
2022-05-22 18:31:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,5 +64,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').style.width = '10rem';
 });
